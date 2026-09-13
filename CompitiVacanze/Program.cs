6ed84daf01733613c46c2d8806b4ae5045e48470
giuse-pio <giupeppepio7@gmail.com>
2026-09-13 18:31:14 +0200

    esercizi 4.2, 4.4 e 5.2 divisi in vari file

--- CompitiVacanze/Program.cs
+++ CompitiVacanze/Program.cs
@@ -1,7 +1,4 @@
 ﻿using LibraryN3;
-using LibraryN4._2;
-using LibraryN4_4;
-using Library5_2;
 namespace CompitiVacanze
 
 {
@@ -23,65 +20,6 @@ namespace CompitiVacanze
             Console.WriteLine($"Nome: {a.Nome}");
             Console.WriteLine($"Cognome: {a.Cognome}");
 
-
-
-            ///* ESERCIZIO 4.2 */
-            Console.WriteLine("inserisci i valori dei quadrati, seguiti da Q#:");
-            string inputq1 = Console.ReadLine();
-            Quadrato q1 = Quadrato.Parse(inputq1);
-
-            string inputq2 = Console.ReadLine();
-            Quadrato q2 = Quadrato.Parse(inputq2);
-
-            Quadrato somma = q1 + q2;
-            Quadrato differenza = q1 - q2;
-
-            Console.WriteLine($"Primo quadrato: {q1}");
-            Console.WriteLine($"Secondo quadrato: {q2}");
-            Console.WriteLine($"Somma: {somma}");
-            Console.WriteLine($"Differenza: {differenza}");
-            Console.WriteLine($"Uguaglianza: {q1 == q2}");
-
-            /* ESERCIZIO 4.4 */
-            Console.WriteLine("inserisci i valori dei Cerchi, seguiti da C^:");
-            string inputc1 = Console.ReadLine();
-            Cerchio c1 = Cerchio.Parse(inputc1);
-
-            string inputc2 = Console.ReadLine();
-            Cerchio c2 = Cerchio.Parse(inputc2);
-
-            Cerchio sommaC = c1 + c2;
-            Cerchio differenzaC = c1 - c2;
-
-            Console.WriteLine($"Primo cerchio: {c1}");
-            Console.WriteLine($"Secondo cerchio: {c2}");
-            Console.WriteLine($"Somma: {sommaC}");
-            Console.WriteLine($"Differenza: {differenzaC}");
-            Console.WriteLine($"Uguaglianza: {c1 == c2}");
-
-
-
-            /* ESERCIZIO 5.2 */
-            Console.Write("Inserisci il primo triangolo (es. Tv5): ");
-            string input1 = Console.ReadLine();
-            Console.Write("Inserisci il secondo triangolo (es. Tv3): ");
-            string input2 = Console.ReadLine();
-            bool t1Valido = Triangolo.TryParse(input1, out Triangolo t1);
-            bool t2Valido = Triangolo.TryParse(input2, out Triangolo t2);
-
-            if (t1Valido && t2Valido)
-            {
-                Triangolo sommaTriangolo = t1 + t2;
-                Console.WriteLine($"Primo triangolo: {t1}");
-                Console.WriteLine($"Secondo triangolo: {t2}");
-                Console.WriteLine($"Somma aree: {sommaTriangolo}");
-                Console.WriteLine($"t1>t2? {t1 > t2}");
-                Console.WriteLine($"Uguaglianza {t1 == t2}");
-            }
-            else
-            {
-                Console.WriteLine("\nErrore: Uno o entrambi i valori inseriti non sono rappresentazioni valide di un triangolo (devono iniziare con 'Tv').");
-            }
         }
 
     }
